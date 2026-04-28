2785f23854e090d24c665e0eee5df73b69956030
John Sehinson <John.Sehinson@kindredgroup.com>
2026-04-28 18:51:30 +0100

    Update Notes

--- dice-roller-web/notes/Guide.js
+++ dice-roller-web/notes/Guide.js
@@ -7,6 +7,11 @@
 // remember to 'cd dice-roller-web' otherwise you will get terminal errors
 // test (localhost:5173)
 npm run dev
-// bundle
+// bundle (5.1.2 only)
 npm run build
-
+// bundle (5.1.3)
+// Github only generates one URL per repository. Since we have all our Vite projects in one all projects share that one URL so we go into .github/workflows/static.yml and change respective paths accordingly
+// i.e. references to project name
+working-directory: dice-roller-web
+working-directory: dice-roller-web
+path: dice-roller-web/dist
