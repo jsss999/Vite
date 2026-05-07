fb2fcca4546561dd19dd1c34cbcf96066c0c2b46
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-07 17:31:37 +0100

    Test remove from main.js

--- dice-roller-web/src/features/uiHandlers.js
+++ dice-roller-web/src/features/uiHandlers.js
@@ -1,16 +1,5 @@
 // /src/features/uiHandlers.js
 import { addEvent } from "../storage/eventStorage";
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.getElementById("myCustom")?.addEventListener("click", function () {
-//         addEvent({
-//             event: "gtm_button_click",
-//             payload: {
-//                 custom: this.getAttribute("data-custom")
-//             }
-//         });
-//     });
-// });
-
 document.getElementById("myCustom")?.addEventListener("click", function () {
     addEvent({
         event: "gtm_button_click",
--- dice-roller-web/src/main.js
+++ dice-roller-web/src/main.js
@@ -3,8 +3,6 @@ import { eventBus } from "./events/eventBus";
 import { sendEventMA } from "./bridge/sendEventMA";
 import "./features/L6L";
 import "./features/RRR10";
-import "./features/uiHandlers";
-// import "./features/diceRules";
 // import "./features/uiHandlers";
 window.eventBus = eventBus;
 window.sendEventMA = sendEventMA;
