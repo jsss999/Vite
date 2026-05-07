2fe201c8db223e69f1202a9de1f9b44245b4252d
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-07 17:18:43 +0100

    Add uiHandler for button Click to Local Storage

--- dice-roller-web/src/features/uiHandlers.js
+++ dice-roller-web/src/features/uiHandlers.js
@@ -1,1 +1,9 @@
 // /src/features/uiHandlers.js
+document.getElementById("myCustom")?.addEventListener("click", function () {
+    addEvent({
+        event: "gtm_button_click",
+        payload: {
+            custom: this.getAttribute("data-custom")
+        }
+    });
+});
