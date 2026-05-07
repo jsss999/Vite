5bcf7799eec4ceb55980525029b217c502daeb28
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-07 16:39:10 +0100

    Add Local Storage

--- dice-roller-web/src/bridge/sendEventMA.js
+++ dice-roller-web/src/bridge/sendEventMA.js
@@ -2,23 +2,6 @@
 import { eventBus } from "../events/eventBus";
 export const sendEventMA = (payload) => {
     console.log("sendEventMA called", JSON.stringify(payload));
-    // const body = {
-    //     tenant: 1372,
-    //     event: "test_event",
-    //     context: {
-    //         event_number: 1710259200,
-    //         event_string: "whwv2app_to_optimove_via_sdk_manifest", // web-hosted assets
-    //         event_text: payload.event || "",
-    //         event_boolean: true,
-    //         // ...payload
-    //     },
-    //     visitor: "065e8f25f620e323",
-    //     customer: "118702737",
-    //     timestamp: new Date().toISOString()
-    // };
-    // if (window.AndroidBridge?.sendEventOpt) {
-    //     window.AndroidBridge.sendEventOpt(JSON.stringify(body));
-    // }
     eventBus.dispatch(payload.event, payload); // forward into internal event bus
     // console.log(`eventBus: ${JSON.stringify(eventBus.getEvents())}`);
 };
