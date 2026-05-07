6166151319ff8c6ca3a3ba550625a3721985da30
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-07 17:22:20 +0100

    bind after DOM load

--- dice-roller-web/src/features/uiHandlers.js
+++ dice-roller-web/src/features/uiHandlers.js
@@ -1,9 +1,11 @@
 // /src/features/uiHandlers.js
-document.getElementById("myCustom")?.addEventListener("click", function () {
-    addEvent({
-        event: "gtm_button_click",
-        payload: {
-            custom: this.getAttribute("data-custom")
-        }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("myCustom")?.addEventListener("click", function () {
+        addEvent({
+            event: "gtm_button_click",
+            payload: {
+                custom: this.getAttribute("data-custom")
+            }
+        });
     });
 });
