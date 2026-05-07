be26d1a762a328bc03fcabc1a4cf204efcdf2a5f
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-07 17:25:11 +0100

    Import uiHandlers

--- dice-roller-web/src/features/uiHandlers.js
+++ dice-roller-web/src/features/uiHandlers.js
@@ -1,11 +1,9 @@
 // /src/features/uiHandlers.js
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("myCustom")?.addEventListener("click", function () {
-        addEvent({
-            event: "gtm_button_click",
-            payload: {
-                custom: this.getAttribute("data-custom")
-            }
-        });
+document.getElementById("myCustom")?.addEventListener("click", function () {
+    addEvent({
+        event: "gtm_button_click",
+        payload: {
+            custom: this.getAttribute("data-custom")
+        }
     });
 });
