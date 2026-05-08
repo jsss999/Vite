6b5441ac6cbc3e6f12b3a4a1ffb2657df2e25b72
John Sehinson <John.Sehinson@kindredgroup.com>
2026-05-08 12:54:39 +0100

    Add dataLayer push

--- dice-roller-web/src/bridge/sendEventMA.js
+++ dice-roller-web/src/bridge/sendEventMA.js
@@ -5,5 +5,7 @@ export const sendEventMA = (payload) => {
     console.log("sendEventMA called", JSON.stringify(payload));
     eventBus.dispatch(payload.event, payload); // forward into internal event bus
     addEvent({ event: payload.event, payload });
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({ event: payload.event, payload });
     // console.log(`eventBus: ${JSON.stringify(eventBus.getEvents())}`);
 };
